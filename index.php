a68ed0ebb995765bbed4dba95879facc270b472a
Lenovo <asadbek.polatov.02@mail.ru>
2022-09-07 14:28:27 +0500

    Districts page compileted

--- index.php
+++ index.php
@@ -53,13 +53,13 @@ if ($text == "/start") {
                     showMain();
                     break;
                 default:
-                    if (in_array(substr($text,5), getDistricts($chat_id))){
+                    if (in_array(substr($text, 5), getDistricts($chat_id))){
                         showSubjects();
                     }
                     else{
                         $telegram->sendMessage([
                            'chat_id' => $chat_id,
-                           'text' => $text,
+                           'text' => substr($text, 5),
                         ]);
                     }
                     break;
