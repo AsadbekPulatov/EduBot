81a50506d8d2fb2fa5fbd3692b57917aa45dde29
Lenovo <asadbek.polatov.02@mail.ru>
2022-09-07 11:16:17 +0500

    language page compileted

--- connect.php
+++ connect.php
@@ -7,6 +7,7 @@ $database = "heroku_7784e9053df43f3";
 
 $connect = new mysqli($servername, $username, $password, $database);
 
+$connect->set_charset("utf8");
 //if ($connect->connect_error) {
 //    die("Connection failed: " . $connect->connect_error);
 //}
