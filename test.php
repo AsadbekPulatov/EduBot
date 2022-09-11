b7511db8b64f48b1562542376d48974fbf0fb916
Lenovo <asadbek.polatov.02@mail.ru>
2022-09-11 09:12:02 +0500

    TrainingCenter created inline button

--- test.php
+++ test.php
@@ -2,31 +2,36 @@
 
 require_once 'User.php';
 
-//$chat_id = 967469906;
-//$user = new User($chat_id);
+$chat_id = 967469906;
+$user = new User($chat_id);
 
 
-$sql = "SELECT * FROM districts WHERE `uz` = 'Yunusobod tumani'";
+$sql = "select * from users where chat_id=" . $chat_id . " limit 1";
+$result = $connect->query($sql)->fetch_assoc();
+$district_id = $result['district_id'];
+$subject_id = $result['subject_id'];
+$sql = "select keyword from subjects where id=" . $subject_id . " limit 1";
+$result = $connect->query($sql)->fetch_assoc();
+$subject = $result['keyword'];
+$sql = "select keyword from districts where id=" . $district_id . " limit 1";
+$result = $connect->query($sql)->fetch_assoc();
+$district = $result['keyword'];
+$sql = "select * from trainingcentres WHERE district = {$district}";
 $result = $connect->query($sql);
-$row = $result->fetch_assoc();
+$centers = [];
+while ($row = $result->fetch_assoc()) {
+    $subjects = explode(',', $row['subjects']);
+    if (in_array($subject, $subjects)) {
+        $centers[] = $row;
+    }
+}
 
-echo 'DISTRICTS<br>';
-echo '<pre>';
-var_dump($row);
-echo '</pre>';
 
-$sql = "SELECT * FROM subjects WHERE `uz` = 'Shaxmat'";
-$result = $connect->query($sql);
-$row = $result->fetch_assoc();
-
-echo 'SUBJECT<br>';
-echo '<pre>';
-var_dump($row);
-echo '</pre>';
-
+echo "district {$district}<br>";
+echo "subject {$subject}<br>";
 
 //$centers = $user->getTrainingCentres();
-//
-//echo '<pre>';
-//var_dump($centers);
-//echo '</pre>';
+
+echo '<pre>';
+var_dump($centers);
+echo '</pre>';
