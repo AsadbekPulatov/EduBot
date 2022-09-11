a434925353b3504bb356e98b6d458776f726b3b3
Lenovo <asadbek.polatov.02@mail.ru>
2022-09-11 08:17:01 +0500

    TrainingCenter created inline button

--- test.php
+++ test.php
@@ -6,5 +6,12 @@ $user = new User("967469906");
 
 $centers = $user->getTrainingCentres();
 
+$sql = "select * from trainingcentres WHERE district = 1";
+$result = $connect->query($sql);
+$centers = [];
+while ($row = $result->fetch_assoc()) {
+    $centers[] = $row;
+}
+
 echo "<pre>".
 var_dump($centers)."</pre>";
