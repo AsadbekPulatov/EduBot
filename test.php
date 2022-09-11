4365b75525cb73b9b0a9a156bb68ebb19af607c6
Lenovo <asadbek.polatov.02@mail.ru>
2022-09-11 08:51:32 +0500

    TrainingCenter created inline button

--- test.php
+++ test.php
@@ -2,11 +2,31 @@
 
 require_once 'User.php';
 
-$chat_id = 967469906;
-$user = new User($chat_id);
+//$chat_id = 967469906;
+//$user = new User($chat_id);
 
-$centers = $user->getTrainingCentres();
 
+$sql = "SELECT * FROM districts WHERE `uz` = 'Yunusobod tumani'";
+$result = $connect->query($sql);
+$row = $result->fetch_assoc();
+
+echo 'DISTRICTS<br>';
 echo '<pre>';
-var_dump($centers);
+var_dump($row);
 echo '</pre>';
+
+$sql = "SELECT * FROM subjects WHERE `uz` = 'Shaxmat'";
+$result = $connect->query($sql);
+$row = $result->fetch_assoc();
+
+echo 'SUBJECT<br>';
+echo '<pre>';
+var_dump($row);
+echo '</pre>';
+
+
+//$centers = $user->getTrainingCentres();
+//
+//echo '<pre>';
+//var_dump($centers);
+//echo '</pre>';
